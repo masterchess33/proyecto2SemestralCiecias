cf8a1150d1313db1604ad235af49641d18e8e2a3
Pablo luer <pablo.luer@icloud.com>
2019-06-06 12:44:04 -0300

    #4 -> se agrego algunas comprobaciones

--- src/main/java/Paquetes/Algoritmo.java
+++ src/main/java/Paquetes/Algoritmo.java
@@ -2,18 +2,24 @@ package Paquetes;
 
 
 public class Algoritmo {
-    private static final char L_PAREN    = '(';
-    private static final char R_PAREN    = ')';
+    private static final char L_paren    = '(';
+    private static final char R_paren    = ')';
     
     public String comprobar(int largo, String cadena){
         String resultado="";
         if(largo!=cadena.length()){
             System.out.println("El largo de la cadena no es el mismo");
-        }else if(cadena.charAt(0)==(')')){
+        }else if(largo>300000){
+            System.out.println("La cadena es demasiada larga");
             resultado=":(";
-        }else if(cadena.charAt(largo-1)==('(')){
+        }else if(cadena.charAt(0)==(R_paren)){
             resultado=":(";
-        
+        }else if(cadena.charAt(1)==(L_paren)){
+        resultado=":(";
+        }else if(cadena.charAt(largo-2)==(R_paren)){
+        resultado=":(";
+        }else if(cadena.charAt(largo-1)==(L_paren)){
+            resultado=":(";
         }else if(largo%2!=0){
         resultado=":(";
         }
