57b6f5e9a682ee4899762e5dd941369b1b0e1410
Pablo luer <pablo.luer@icloud.com>
2019-06-06 12:31:16 -0300

    #4 -> se agrego algunas comprobaciones

--- src/main/java/Paquetes/Menu.java
+++ src/main/java/Paquetes/Menu.java
@@ -7,14 +7,19 @@ import java.util.Scanner;
 public class Menu {
     
     public void inicio() {
-    boolean estado = true;
+        Algoritmo al=new Algoritmo();
+        boolean estado = true;
     while (estado) {
         System.out.println("Elija una opción");
         System.out.println("1. Iniciar programa");
         System.out.println("2. Salir");
         int opcion = recibirValidar();
         switch (opcion) {
-            case 1:
+            case 1: System.out.println("Ingrese el lago de la cadena");
+                    int largo=recibirValidar();
+                    System.out.println("Ingrese la cadena(solo '(' ,')' y '?')");
+                    String cadena=recibirPalabra();
+                    al.comprobar(largo, cadena);
 
                 break;
 
@@ -41,6 +46,27 @@ public static int recibirValidar() {
 
         } catch (InputMismatchException e) {
             System.out.println("El caracter ingresado no es numerico o se encuentra fuera del rango establecido, intentelo nuevamente.");
+            estado = true;
+        }
+
+    }
+    return num;
+}
+
+public static String recibirPalabra() {
+    String num = " ";
+    boolean estado = true;
+
+    while (estado) {
+
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            num = sc.nextLine();
+            estado = false;
+
+        } catch (InputMismatchException e) {
+            System.out.println("El caracter ingresado no es una palabra, intentelo nuevamente.");
             estado = true;
         }
 
